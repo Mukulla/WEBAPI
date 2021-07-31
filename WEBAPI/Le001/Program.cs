3a257bee55ccf3a0d40cd4e1de8bbd8bdc94f513
Maxim <mbzozn@mail.ru>
2021-07-31 12:10:12 +0300

    Lesson001 added

---
+++ WEBAPI/Le001/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace Le001
+{
+    class Program
+    {        
+        static readonly HttpClient client = new HttpClient();
+        private static readonly CancellationTokenSource cts = new CancellationTokenSource();
+
+        static string Way = "https://jsonplaceholder.typicode.com/posts/";
+
+        static void Main(string[] args)
+        {            
+            Execute();
+            Console.Read();
+        }
+
+        static async void Execute()
+        {
+            for ( int i = 4; i < 14; ++i )
+            {
+                ExecuteOnce(i);
+                Console.WriteLine();
+            }
+        }
+        
+        static async void ExecuteOnce(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync($"{Way}{id}");                
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);                
+                cts.CancelAfter(100);
+                return;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
